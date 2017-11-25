5b18bd4902223b1dfe6eefb41f804a8e7d0a4db2
Titejue <juliette.mendras@gmail.com>
2017-11-25 18:22:55 +0100

    Modif Methode choisir Carte du Joueur

--- src/fr/utt/lo02/Joueur.java
+++ src/fr/utt/lo02/Joueur.java
@@ -19,6 +19,7 @@ public abstract class Joueur {
     /**
      * Un joueur peut :
      * Piocher
+     * Choisir une Carte
      * Poser une carte
      * Passer son tour
      * Regarder le jeu dans main (méthode qui appelle une méthode afficher() de la classe main
@@ -28,42 +29,66 @@ public abstract class Joueur {
         main.add(carte);
     }
 
-    // -------------------------------------------- POSER CARTE -----------------------------------------------
+    // -------------------------------------------- CHOISIR CARTE -----------------------------------------------
 
-    
-    public Carte posercarte() {
+
+    public Carte choisirCarte() {
+
         // Il faut créer un objet "carteChoisie" qui est l'une des cartes composant la main du joueur
         // Il va falloir demander par un terminal quelle carte jouer
+
         /**
          * 1) Affichage de la main
          * 2) Choix de l'une des cartes de la main du joueur
          * 3) On enlève la carteChoisie de la main du joueur et on l'ajoute au tapis
          */
+
         // Possibilité d'afficher les cartes de la main
+
         /** Methode à coder : en attende de la classe Main ?*/
+
         // On demande quelle carte jouer
         // On part du principe que carte prend en paramètre deux String : couleur et valeur ?
+
         Scanner cartePourJouer = new Scanner(System.in);
+
         System.out.println("Il faut choisir une carte à jouer ! \t Suivez les instructions :  ");
         System.out.println("Veuillez Saisir la valeur de la carte à jouer ");
-        String carteValeur = cartepourJouer.nextLine();
+        String carteValeur = cartePourJouer.nextLine() ;
+
         // On vide la ligne avant d'en lire une autre
         cartePourJouer.nextLine();
+
         System.out.println("Veuillez Saisir la couleur de la carte à jouer ");
-        String carteCouleur = cartepourJouer.nextLine();
-        System.out.println("Vous jouez la carte" + str);
-        Carte carteChoisie = new Carte(x);
+        String carteCouleur = cartePourJouer.nextLine();
+
+        //On affiche la carte choisie
+        System.out.println("Vous jouez la carte : " + carteValeur + " de " + carteCouleur);
+
+        /**
+         * Pour trouver la carte : recherche dans la main du joueur de la carte en question
+         * Création de l'objet carteChoisie de type carte et défausse de la main du joueur
+         * IL FAUT VERIFIER COMMENT PY DECLARE LE CONSTRUCTEUR CARTE !!!
+         */
+        Carte carteChoisie = new Carte(carteValeur, carteCouleur);
+
+        // On supprime la carteChoisie de la main du joueur
         main = main.remove(carteChoisie);
-        //System.out.println(carteChoisie);
+
+        // On peut choisir de retourner la carteChoisie ou on prend une methode void ?
         return carteChoisie;
+
+        // Il faudrait appeler une méthode poser sur la defausse ou le talon pour gérer les listes !
     }
-    /
-    On pose
-    la 1
-    ère carte
-    du deck
-    sur la
-    défausse
+
+
+    // -------------------------------------------- POSER SUR LE TALON ------------------------------------------
+
+    public
+
+
+
+    //  On pose la 1ère carte du deck sur la défausse
     Carte carte = this.liste.getFirst();
     // On supprime la carte du paquet de cartes du joueur
         this.liste.remove(carte);
