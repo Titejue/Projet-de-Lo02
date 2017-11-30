df1a7d272f329c73b87bd38a167e8fc89b92f1db
Titejue <juliette.mendras@gmail.com>
2017-11-30 13:43:09 +0100

    Constructeur terminé

--- src/fr/utt/lo02/Partie.java
+++ src/fr/utt/lo02/Partie.java
@@ -7,63 +7,94 @@ import java.lang.* ;
 public class Partie {
 
     // Declaration d'un singleton
-    private static Partie ourInstance =  null;
+
+    private static Partie ourInstance = null ;
 
     // Attributs de la classe
 
     private boolean fini = false;
 
     private static LinkedList<Joueur> joueurs;
+    private LinkedList<Carte> dernièresCartes;
+    private LinkedList<Carte> listePioche ;
 
-    private int nbJoueur;
-    private int numVar;
+
+    private int nbJoueur = 0;
+    private int numVar = 5 ;
     private int typeDeJeu ;
     private int nbDeck ;
-    private Variante variante;
-    private JeuDeCarte jeuCarte;
+    private Variante variante ;
+    private JeuDeCarte jeuCarte ;
+    private Carte derniereCarte ;
+    private Pioche pioche ;
+    private Talon talon ;
+    private int nombreDeCarteDeck ;
+    private int nombreCarteDistribuer ;
+
 
     // ------------------------ CONSTRUCTEUR --------------------------------------
 
     /**
      * Il faudrait plutot se demander si le nombre de joueur ne devrait pas etre choisi par le joueur lui meme
      */
-    private Partie() {
+    public Partie() {
 
         //On instancie tout les paramètres : Variante, jeuDeCarte et la liste de Joueurs :
         this.joueurs = new LinkedList<>() ;
 
-        System.out.println("Démarrage d'une partie");
-        Partie partie = Partie.getInstance();
+        System.out.println("Démarrage d'une partie") ;
 
-        System.out.println("Veuillez saisir votre nom");
-        Scanner sc = new Scanner(System.in);
-        String nomJoueur = sc.nextLine();
-        joueurs.add(new JoueurReel(nomJoueur));
+        System.out.println("Veuillez saisir votre nom") ;
+        Scanner sc = new Scanner(System.in) ;
+        String nomJoueur = sc.nextLine() ;
+        joueurs.add(new JoueurReel(nomJoueur)) ;
+        this.nbJoueur++ ;
 
-        System.out.println("Combien de joueur dans la partie (vous compris)");
-        this.nbJoueur = sc.nextInt();
-        for (int j = 1; j < nbJoueur; j++) {
-            joueurs.add(new Adversaire("adversaire" + j));
+        while (this.nbJoueur < 2) {
+            System.out.println("Combien de joueur dans la partie (vous compris)");
+            this.nbJoueur = sc.nextInt();
+            if (nbJoueur < 2){
+                System.out.println("Le nombre de joueurs minimal est 2 !") ;
+            }
+            else {
+                for (int j = 1; j < nbJoueur; j++) {
+                    joueurs.add(new Adversaire("adversaire" + j));
+                }
+            }
         }
 
         // On demande les données nécessaires : type de jeu, nombre de Decks
-        System.out.println("Avec quel Type de Jeu souhaitez-vous joueur ?\t 0 : Jeu classique 54 cartes (52 cartes + 2 Jokers)\t 1 : Jeu de 52 cartes\t 2 : Jeu de 34 cartes (32 cartes + 2 Jokers)\t 3 : Jeu de 32 cartes");
-        this.typeDeJeu = sc.nextInt();
+        while (this.typeDeJeu > 3) {
+            if (nbJoueur > 3){
+                System.out.println("Le numéro sélectionné n'est pas répertorié  ! \n") ;
+            }
+            else {
+                for (int j = 1; j < nbJoueur; j++) {
+                    joueurs.add(new Adversaire("adversaire" + j));
+                }
+            }
+
+            System.out.println("Avec quel Type de Jeu souhaitez-vous joueur ?\n 0 : Jeu classique 54 cartes (52 cartes + 2 Jokers)\n 1 : Jeu de 52 cartes\n 2 : Jeu de 34 cartes (32 cartes + 2 Jokers)\n 3 : Jeu de 32 cartes");
+            this.typeDeJeu = sc.nextInt();
+        }
         System.out.println("Veuillez choisir le nombre de jeu de Carte de ce type pour la partie : ");
         this.nbDeck = sc.nextInt();
 
         // On crée le jeu de carte de type JeuDeCarte
         this.jeuCarte = new JeuDeCarte(typeDeJeu, nbDeck);
 
-        System.out.println("Veuillez choisir la variante :\t ");
-        System.out.println("0 : Variante Classique \t 1 : Version de MonClar \t 2 : Version Minimale \t 3 : Variante 1 \t 4 : Variante Carte et Maou");
-        this.numVar = sc.nextInt();
+
+        while (this.numVar > 4) {
+            System.out.println("Veuillez choisir la variante : ");
+            System.out.println("0 : Variante Classique \n 1 : Version de MonClar \n 2 : Version Minimale \n 3 : Variante 1 \n 4 : Variante Carte et Maou");
+            this.numVar = sc.nextInt();
+        }
 
         /** Numéro de Variante
          * 0 : Variante Classique
          * 1 : Version de MonClar
          * 2 : Version Minimale
-         * 3 : Variante
+         * 3 : Variante 1
          * 4 : Variante Carte et Maou
          */
 
@@ -72,7 +103,7 @@ public class Partie {
             case(0) :
                 this.variante = new VarianteClassique() ;
                 break ;
-            case(1) :
+            /**case(1) :
                 this.variante = new VarianteMonClar();
                 break ;
             case(2) :
@@ -80,38 +111,88 @@ public class Partie {
                 break ;
             case(3) :
                 this.variante  = new Variante1() ;
-                break ;
+                break ;**/
+            default:
         }
 
         // On crée la pioche une fois par partie
-        Pioche pioche = new Pioche(jeuCarte);
+        this.pioche = new Pioche(jeuCarte) ;
         // On crée le Talon une fois par partie
-        Talon talon = new Talon();
+        this.talon = new Talon() ;
+
+
+        // On choisit le nombre de carte à distribuer
+        if (numVar == 0 || numVar == 1 || numVar == 2 || numVar == 3 || numVar == 4 ){
+            if (nbJoueur == 2) {
+                if (nbDeck == 1) {
+                    this.nombreCarteDistribuer = 10;
+                } else {
+                    this.nombreCarteDistribuer = 15;
+                }
+            }
+            else if (nbJoueur == 3){
+                if (nbDeck == 1) {
+                    this.nombreCarteDistribuer = 8;
+                } else {
+                    this.nombreCarteDistribuer = 12;
+                }
+            }
+            else {
+                if (nbDeck == 1) {
+                    this.nombreCarteDistribuer = 6;
+                } else {
+                    this.nombreCarteDistribuer = 9;
+                }
+            }
+        }
+
+        // On distribue les cartes
+        for (int i=0 ; i<joueurs.size() ; i++) {
+            pioche.donnerCarte(joueurs.get(i), 1);
+        }
+
+        // On dépose une carte de la pioche sur le talon
+        talon.recevoirCarte(pioche.getPremiereCarte()) ;
+
+
     }
+
+    /**
+     * this.listePioche = pioche.getPioche() ;
+     * this.nombreDeCarteDeck = listePioche.size() ;
+     */
 
 
 
 // ---------------------------------- GETTER ET SETTER ---------------------------------------------------
 
 
-    public static LinkedList<Joueur> getJoueurs() {
+    public LinkedList<Joueur> getJoueurs() {
         return joueurs;
     }
 
     public static Partie getInstance() {
         if (ourInstance == null){
-            ourInstance = new Partie(); // On mettra les paramètres plus tard
-            return ourInstance;
+            ourInstance = new Partie();
         }
-        else {
-            System.out.println("Erreur : une partie est déja instanciée");
-        }
+        return ourInstance ;
     }
 
 
 
 
-    // -------------------------------- PIOCHER UNE OU PLUSIEURS CARTE(S)
+
+    // -------------------------------- GERER UN TOUR ---------------------------------
+
+
+    // On enregistrera les valeurs des cartes jouées dans une linkedList qui ne gardera qu'en mémoire que les x dernières cartes jouées.
+    // x étant le nombre de joueurs dans la parties
+
+
+
+
+
+
 
     /**
      * Le joueur va recevoir un "entier" en pénalité
